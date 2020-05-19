74eb73d698e3200fe47dfd94fd4f19d097fad6aa
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2020-05-19 08:42:32 +0100

    fix registers

--- chip8.h
+++ chip8.h
@@ -6,7 +6,7 @@
 
 #define MEMORY_SIZE_BYTES (2 << 11) /* 4K */
 #define PROGRAM_START 0x200
-#define MAX_STACK_DEPTH 32
+#define MAX_STACK_DEPTH 16
 /* #define FREQUENCY 10      /\* Hz *\/ */
 #define FREQUENCY 60      /* Hz */
 #define USECONDS_PER_STEP (1000000 / FREQUENCY) /* Seconds per step */
@@ -20,16 +20,28 @@ enum {
 } state;
 
 typedef struct chip8 {
+    /* Vx - general purpose registers, VF for flags  */
     uint8_t V0, V1, V2, V3, V4, V5, V6, V7, V8, V9, VA, VB, VC, VD, VE, VF;
 
-    uint8_t I;
+    /* Mostly for memory addresses, only 12 lower bits used */
+    uint16_t I;
+    /* Program counter */
     uint16_t PC;
+
+    /* Delay timer, 60 Hz */
     uint8_t DT;
+    /* Sound timer, 60 Hz */
     uint8_t ST;
 
-    uint8_t ram[MEMORY_SIZE_BYTES];
-    uint8_t stack[MAX_STACK_DEPTH];
+    /* Stack pointer */
+    uint8_t SP;
+    /* Stack */
+    uint16_t stack[MAX_STACK_DEPTH];
 
+    /* Memory */
+    uint8_t ram[MEMORY_SIZE_BYTES];
+
+    /* Framebuffer, previous and new */
     uint8_t fb[FRAMEBUF_SIZE];
     uint8_t fb_old[FRAMEBUF_SIZE];
 
